af35bd21d0fcf7088321db7157a989bcdcf30c86
Nica Rivera <nclangela.rvr@gmail.com>
2015-02-05 09:25:39 -0800

    Added files

---
+++ js/style.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+ var wHeight = $(window).height();
+ var stickToBot = wHeight - $('.nav').outerHeight(true);
+ $('#about').hide();
+ $('#works').hide();
+ $('#contact').hide();
+ $('.nav').hide();
+ $('.nav-logo').hide();
+ $('.nav').css({'top': stickToBot + 'px'});
+ $(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 30) {
+    $('.nav').fadeIn();
+    $('.scroll').fadeOut();
+  } if ( y > 180) {
+ $('#about').fadeIn();
+
+  } if ( y > 840 ) {
+  $('#works').fadeIn();
+  }
+  if ( y > 2010 ) {
+  $('#contact').fadeIn();
+  }
+
+  if (y == 0) {
+    $('.scroll').fadeOut();
+  }
+
+
+$(window).scroll(function() {
+   var scrollVal = $(this).scrollTop();
+   var state = true
+   if ( scrollVal > stickToBot ) {
+    $('.nav').css({'position':'fixed','top' :'0px', 'background':'white'});
+    $('ul li').css({'font-size':'1em'});
+    $('.nav-logo').fadeIn();
+
+  }
+  else {
+   $('.nav').css({'position':'absolute','top': stickToBot +'px', 'background':'none'});
+    $('ul li').css({'font-size':'1.2em'});
+    $('.nav-logo').fadeOut();
+
+    $(document).ready(function() {
+
+       function setHeight() {
+        var windowHeight = $(window).innerHeight();
+        $('body').css('min-height', windowHeight);
+        $('#home').css('min-height', windowHeight);
+      };
+      setHeight();
+
+      $(document).load(function () { $(window).resize(function() {
+        setHeight();
+      });
+});
+
+    });
+  }
+});
+});
+
+});
+
